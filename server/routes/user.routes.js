d413904118d9b714e8f0b57e5d70f0033082e7f9
Nitish chaurasiya <nitishchaurasiyac555@gmail.com>
2023-09-27 16:40:34 +0530

    user can get there details now

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { getProfile, login, logout, register } from "../controller/user.controller.js";
+import { isLoggedIn } from "../middleware/auth.middleware.js";
 
 const router = Router()
 
@@ -7,7 +8,7 @@ const router = Router()
 router.post('/register',register)
 router.post('/login',login)
 router.get('/logout',logout)
-router.get('/me',getProfile)
+router.get('/me',isLoggedIn,getProfile)
 
 
 export default  router
