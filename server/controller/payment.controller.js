cbf59a04a1596b828ac4f09f6f83435e09b5320f
Nitish chaurasiya <nitishchaurasiyac555@gmail.com>
2023-09-30 16:56:33 +0530

    razorpay payment model and routes added

---
+++ server/controller/payment.controller.js
@@ -0,0 +1,23 @@
+
+
+
+const getRazorpayApiKey = async(req,res,next)=>{
+    
+}
+const buySubscription = async(req,res,next)=>{
+
+}
+const verifySubscription = async(req,res,next)=>{
+
+}
+const cancelSubscription = async(req,res,next)=>{
+
+}
+const allPayments = async(req,res,next)=>{
+
+}
+
+
+export {
+    getRazorpayApiKey,buySubscription,verifySubscription,cancelSubscription,allPayments
+}
