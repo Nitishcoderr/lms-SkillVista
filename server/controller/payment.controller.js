df9531e0047784609e86f72b274de8fd2e327854
Nitish chaurasiya <nitishchaurasiyac555@gmail.com>
2023-10-01 22:03:57 +0530

    to buy subscription ctrl created

--- server/controller/payment.controller.js
+++ server/controller/payment.controller.js
@@ -1,3 +1,7 @@
+import User from "../models/user.model.js";
+import { razorpay } from "../server.js";
+import AppError from "../utils/error.util.js";
+
 
 const getRazorpayApiKey = async(req,res,next)=>{
     res.status(200).json({
@@ -7,7 +11,37 @@ const getRazorpayApiKey = async(req,res,next)=>{
     })
 }
 const buySubscription = async(req,res,next)=>{
-    
+    const {id} = req.user;
+    const user = await User.findById(id)
+
+    if(!user){
+        return next(
+            new AppError('Unauthorized,please login',404)
+        )
+    }
+
+    if(user.role === 'ADMIN'){
+        return next(
+            new AppError('Admin cannot purchase a subscription',400)
+        )
+    }
+
+    const subscription = await razorpay.subscriptions.create({
+        plan_id:process.env.RAZORPAY_PLAN_ID;
+        customer_notify:1
+    });
+
+    user.subscription.id = subscription.id;
+    user.subscription.status = subscription.status;
+
+    await user.save();
+
+    res.status(200).json({
+        success:true,
+        message:'Subscribed successfully',
+        subscription_id:subscription.id
+    })
+
 }
 const verifySubscription = async(req,res,next)=>{
 
--- server/server.js
+++ server/server.js
@@ -1,15 +1,22 @@
 import app from './app.js'
 import connectionToDb from './config/dbConnection.js';
 import cloudinary from 'cloudinary'
+import Razorpay from 'razorpay'
 
 const PORT = process.env.PORT || 5050
 
 // Cloudinary configuration
-
 cloudinary.v2.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
+// Razorpay config
+
+export const razorpay = new Razorpay({
+    key_id:process.env.RAZORPAY_KEY_ID,
+    key_secret:process.env.RAZORPAY_SECRET
 })
 
 app.listen(PORT, async () => {
