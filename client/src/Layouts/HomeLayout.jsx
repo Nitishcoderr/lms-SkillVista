1cf60f1992fef9c4f36b3040fa4c4e739a46eb63
Nitish chaurasiya <nitishchaurasiyac555@gmail.com>
2023-10-02 18:09:17 +0530

    Added login signup button to sidebar

--- client/src/Layouts/HomeLayout.jsx
+++ client/src/Layouts/HomeLayout.jsx
@@ -1,68 +1,128 @@
-import React from 'react'
-import {FiMenu} from 'react-icons/fi'
-import {AiFillCloseCircle} from 'react-icons/ai'
-import { Link } from 'react-router-dom';
-import Footer from '../Component/Footer'
+import React from 'react';
+import { FiMenu } from 'react-icons/fi';
+import { AiFillCloseCircle } from 'react-icons/ai';
+import { Link, useNavigate } from 'react-router-dom';
+import Footer from '../Component/Footer';
+import { useDispatch, useSelector } from 'react-redux';
 
-const HomeLayout = ({children}) => {
+const HomeLayout = ({ children }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
+  // For checking if user is logged in
+  const isLoggedIn = useSelector((state) => state?.auth?.isLoggedIn);
 
-  function changeWidth(){
+  // For displaying the option acc to role
+  const role = useSelector((state) => state?.auth?.auth?.role);
+
+  function changeWidth() {
     const drawerSide = document.getElementsByClassName('drawer-side');
-    drawerSide[0].style.width = "auto";
+    drawerSide[0].style.width = 'auto';
   }
 
-  function hideDrawer(){
+  function hideDrawer() {
     const element = document.getElementsByClassName('drawer-toggle');
     element[0].checked = false;
 
     const drawerSide = document.getElementsByClassName('drawer-side');
-    drawerSide[0].style.width = "0";
+    drawerSide[0].style.width = '0';
+  }
+
+  async function handleLogout(e) {
+    e.preventDefault();
+
+    // const res = await dispatch(logout());
+
+    // if(res?.payload?.success)
+    navigate('/');
   }
 
   return (
-    <div className='min-h-[90vh]'>
-    <div className="drawer absolute left-0 z-50 w-fit">
-    <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-    <div className="drawer-content">
-      <label htmlFor="my-drawer" className="cursor-pointer relative">
-        <FiMenu onClick={changeWidth} size={"32px"} className='font-bold text-white m-4' />
-      </label>
-    </div> 
-    <div className="drawer-side w-0">
-      <label htmlFor="my-drawer" className="drawer-overlay"></label>
-      <ul className="menu p-4 w-48 sm:w-80 bg-base-200 text-base-content relative">
-        {/* Sidebar content here */}
-        <li className='w-fit absolute right-2 z-50'>
-          <button onClick={hideDrawer}>
-            <AiFillCloseCircle size={24} />
-          </button>
-          </li>
-          <li>
-        <Link to='/'>Home</Link>
-          </li>
-          <li>
-        <Link to='/courses'>All Courses</Link>
-          </li>
-          <li>
-        <Link to='/contact'>Contact us</Link>
-          </li>
-          <li>
-        <Link to='/about'>About us</Link>
-          </li>
-        
-      </ul>
+    <div className="min-h-[90vh]">
+      <div className="drawer absolute left-0 z-50 w-fit">
+        <input
+          id="my-drawer"
+          type="checkbox"
+          className="drawer-toggle"
+        />
+        <div className="drawer-content">
+          <label
+            htmlFor="my-drawer"
+            className="cursor-pointer relative">
+            <FiMenu
+              onClick={changeWidth}
+              size={'32px'}
+              className="font-bold text-white m-4"
+            />
+          </label>
+        </div>
+        <div className="drawer-side w-0">
+          <label
+            htmlFor="my-drawer"
+            className="drawer-overlay"></label>
+          <ul className="menu p-4 w-48 sm:w-80 bg-base-200 text-base-content relative">
+            {/* Sidebar content here */}
+            <li className="w-fit absolute right-2 z-50">
+              <button onClick={hideDrawer}>
+                <AiFillCloseCircle size={24} />
+              </button>
+            </li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            {isLoggedIn && role === 'ADMIN' && (
+              <li>
+                <Link to="/admin/dashboard">Admin DashBoard</Link>
+              </li>
+            )}
+            <li>
+              <Link to="/courses">All Courses</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact us</Link>
+            </li>
+            <li>
+              <Link to="/about">About us</Link>
+            </li>
+
+            {!isLoggedIn && (
+              <li className="absolute bottom-4 w-[90%]">
+                <div className="w-full flex items-center justify-center">
+                  <button className="btn-primary px-4 py-1 font-semibold rounded-md w-full">
+                    <Link to="/login">Login</Link>
+                  </button>
+                  <button className="btn-secondary px-4 py-1 font-semibold rounded-md w-full">
+                    <Link to="/login">Signup</Link>
+                  </button>
+                </div>
+              </li>
+            )}
+
+            {!isLoggedIn && (
+              <li className="absolute bottom-4 w-[90%]">
+                <div className="w-full flex items-center justify-center">
+                  <button className="btn-primary px-4 py-1 font-semibold rounded-md w-full">
+                    <Link to="/user/profile">Profile</Link>
+                  </button>
+                  <button className="btn-secondary px-4 py-1 font-semibold rounded-md w-full">
+                    <Link
+                      onClick={handleLogout}
+                      to="/logout">
+                      Logout
+                    </Link>
+                  </button>
+                </div>
+              </li>
+            )}
+          </ul>
+        </div>
+      </div>
+
+      {children}
+
+      <Footer />
     </div>
-  </div>
+  );
+};
 
-{children}
-
-<Footer/>
-
-
-
-    </div>
-  )
-}
-
-export default HomeLayout
+export default HomeLayout;
