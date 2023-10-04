cb9578b69c9387224bc8e25000e59ce565d4cbd7
Nitish chaurasiya <nitishchaurasiyac555@gmail.com>
2023-10-04 17:56:52 +0530

    Prepared details component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Login from './Pages/Login';
 import CourseList from './Pages/Course/CourseList';
 import Contact from './Pages/Contact';
 import Denied from './Pages/Denied';
+import CourseDescription from './Pages/Course/CourseDescription';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route exact path='/courses' element={<CourseList/>} />
         <Route exact path='/contact' element={<Contact/>} />
         <Route exact path='/denied' element={<Denied/>} />
+        <Route exact path='/course/description' element={<CourseDescription/>} />
         <Route  path='*' element={<NotFound/>} />
       </Routes>
     </>
